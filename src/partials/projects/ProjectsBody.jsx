1c8ecf28bc85bcdba91b7080b775d6ef5a05cdb5
md-ashikur <ashik76690@gmail.com>
2023-04-26 12:06:42 +0600

    first commit

---
+++ src/partials/projects/ProjectsBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsBody = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center'>this is project body </h1> 
+        </div>
+    );
+};
+
+export default ProjectsBody;
