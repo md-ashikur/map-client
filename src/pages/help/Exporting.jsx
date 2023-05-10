0576a10fe2393c014322f0ba4d719fd37e13032a
MD Ashikur Rahman <71284265+md-ashikur@users.noreply.github.com>
2023-05-10 20:58:56 +0600

    help export page fix

--- src/pages/help/Exporting.jsx
+++ src/pages/help/Exporting.jsx
@@ -9,20 +9,12 @@ const Exporting = () => {
 
     return (
         <div className="flex h-screen overflow-hidden">
-        {/* Sidebar */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-  
-        {/* Content area */}
-        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-          {/*  Site header */}
-         
-  
-          <main>
+     
+        
             <div className="relative flex m-5">
            export
             </div>
-          </main>
-        </div>
+         
       </div>
     );
 };
